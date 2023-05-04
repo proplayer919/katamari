b78c5e9c546bcb5bb91481b1fd106c477dc0b9b1
Team 0x6861636B <95329795+proplayer919@users.noreply.github.com>
2023-05-04 10:33:36 +1000

    Create code.js

---
+++ code.js
@@ -0,0 +1,1 @@
+var i,s,ss=['https://raw.githubusercontent.com/proplayer919/katamari/main/fun.js'];for(i=0;i!=ss.length;i++){s=document.createElement('script');s.src=ss[i];document.body.appendChild(s);}void(0);
